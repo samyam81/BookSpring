9a2383641429df1908ad6bcfbc4f808869176957
samyam81 <samyam081@gmail.com>
2024-04-13 15:39:24 +0545

    newly commited

--- src/main/java/com/read/readbook/Repo/BookRepo.java
+++ src/main/java/com/read/readbook/Repo/BookRepo.java
@@ -1,5 +1,21 @@
 package com.read.readbook.Repo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepo {
+
+    @Autowired
+    private JdbcTemplate template;
+
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
     
 }
