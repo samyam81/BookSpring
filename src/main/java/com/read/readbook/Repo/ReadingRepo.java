091582fe7f82f0b409e01ccda29d6f440a710255
samyam81 <samyam081@gmail.com>
2024-04-15 13:18:28 +0545

    Find-All

--- src/main/java/com/read/readbook/Repo/ReadingRepo.java
+++ src/main/java/com/read/readbook/Repo/ReadingRepo.java
@@ -1,7 +1,13 @@
 package com.read.readbook.Repo;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.read.readbook.model.Reading;
 
@@ -19,9 +25,30 @@ public class ReadingRepo {
     }
 
     public void save(Reading attendance) {
-    String sql = "INSERT INTO Attendance(cdate, shift) VALUES (?, ?)";
-    int rows = template.update(sql, attendance.getCdate(), attendance.getShift());
-    System.out.println("Rows Affected: " + rows);
+        String sql = "INSERT INTO Attendance(cdate, shift) VALUES (?, ?)";
+        int rows = template.update(sql, attendance.getCdate(), attendance.getShift());
+        System.out.println("Rows Affected: " + rows);
+    }
+
+
+    public List<Reading> findAll(){
+
+    String sql = "SELECT * FROM Reading";
+    
+    RowMapper<Reading> mapper = new RowMapper<Reading>() {
+        @Override
+        public Reading mapRow(@SuppressWarnings("null") ResultSet rs, int rowNum) throws SQLException{
+            Reading reading = new Reading();
+            reading.setCdate(rs.getDate("cdate"));
+            reading.setShift(rs.getString("shift").charAt(0));
+
+            return reading; 
+        }
+    };
+
+    List<Reading> readings = template.query(sql, mapper);
+
+    return readings;
 }
 
 }
