ad22245e4b10b81632ed91744a579bdb1799080e
samyam81 <samyam081@gmail.com>
2024-04-15 13:23:12 +0545

    Selection

--- src/main/java/com/read/readbook/Repo/ReadingRepo.java
+++ src/main/java/com/read/readbook/Repo/ReadingRepo.java
@@ -1,5 +1,7 @@
 package com.read.readbook.Repo;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -7,6 +9,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.RowMapper;
 
 import com.read.readbook.model.Reading;
@@ -57,7 +60,7 @@ public class ReadingRepo {
     
     RowMapper<Reading> mapper = new RowMapper<Reading>() {
         @Override
-        public Reading mapRow(ResultSet rs, int rowNum) throws SQLException{
+        public Reading mapRow(@SuppressWarnings("null") ResultSet rs, int rowNum) throws SQLException{
             Reading reading = new Reading();
             reading.setCdate(rs.getDate("cdate"));
             reading.setShift(rs.getString("shift").charAt(0));
@@ -70,5 +73,60 @@ public class ReadingRepo {
 
     return readings;
 }
+
+public List<Reading> findByDate(Date cdate){
+
+    String sql = "SELECT * FROM Reading WHERE cdate = ?";
+    
+    RowMapper<Reading> mapper = new RowMapper<Reading>() {
+        @Override
+        public Reading mapRow(@SuppressWarnings("null") ResultSet rs, int rowNum) throws SQLException{
+            Reading reading = new Reading();
+            reading.setCdate(rs.getDate("cdate"));
+            reading.setShift(rs.getString("shift").charAt(0));
+
+            return reading; // Returning the Reading object 'reading', not the class name.
+        }
+    };
+
+    List<Reading> readings = template.query(sql, new PreparedStatementSetter() {
+        @Override
+        public void setValues(@SuppressWarnings("null") PreparedStatement ps) throws SQLException {
+            ps.setDate(1, cdate);
+        }
+    }, mapper);
+
+    return readings;
+}
+
+    public List<Reading> findByDateAndShift(Date cdate, char shift){
+
+    String sql = "SELECT * FROM Reading WHERE cdate = ? AND shift = ?";
+    
+    RowMapper<Reading> mapper = new RowMapper<Reading>() {
+        @Override
+        @SuppressWarnings("null")
+        public Reading mapRow( ResultSet rs, int rowNum) throws SQLException{
+            Reading reading = new Reading();
+            reading.setCdate(rs.getDate("cdate"));
+            reading.setShift(rs.getString("shift").charAt(0));
+
+            return reading; // Returning the Reading object 'reading', not the class name.
+        }
+    };
+
+    List<Reading> readings = template.query(sql, new PreparedStatementSetter() {
+        @SuppressWarnings("null")
+        @Override
+        public void setValues(PreparedStatement ps) throws SQLException {
+            ps.setDate(1, cdate);
+            ps.setString(2, String.valueOf(shift));
+        }
+    }, mapper);
+
+    return readings;
+}
+
+
 
 }
