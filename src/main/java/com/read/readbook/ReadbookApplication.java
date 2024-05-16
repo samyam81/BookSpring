33abecd4b090b50d1b308afacd130e737050643c
samyam81 <samyam081@gmail.com>
2024-05-16 18:47:51 +0545

    I quit

--- src/main/java/com/read/readbook/ReadbookApplication.java
+++ src/main/java/com/read/readbook/ReadbookApplication.java
@@ -1,8 +1,12 @@
 package com.read.readbook;
 
+import java.awt.print.Book;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.read.readbook.Repo.LibraryRepo;
 import com.read.readbook.model.Library;
@@ -26,6 +30,15 @@ public class ReadbookApplication {
 		} catch (Exception e) {
 			System.out.println("Error..");
 		}
+
+		ApplicationContext context2 = SpringApplication.run(ReadbookApplication.class, args);
+		Book book1 = context2.getBean(Book.class);
+		book1.setName("The Great Gatsby");
+		book1.setDetail("A novel by F. Scott Fitzgerald");
+		book1.setPages(180);
+		book1.setLanguage("English");
+		book1.setGenre("Fiction");
+		book1.setBookID(123456);
 		
 
 	}
